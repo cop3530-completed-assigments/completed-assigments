9dfd142e230273a3b26cfa04e8273146e6c96ed4
Alejandro Klever <45394625+alejandroklever@users.noreply.github.com>
2025-07-20 22:23:08 -0400

    update assignment 7

--- alejandroklever/assignment_07_merge_sort/src/Main.java
+++ alejandroklever/assignment_07_merge_sort/src/Main.java
@@ -106,34 +106,3 @@ public class Main {
     }
 }
 
-class Student implements Comparable<Student> {
-    private String name;
-    private double gpa;
-
-    public Student(String name, double gpa) {
-        this.name = name;
-        this.gpa = gpa;
-    }
-
-    public double getGpa() {
-        return gpa;
-    }
-
-    @Override
-    public int compareTo(Student other) {
-        return Double.compare(this.gpa, other.gpa);
-    }
-
-    @Override
-    public String toString() {
-        return name + " (" + gpa + ")";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Student student = (Student) obj;
-        return Double.compare(student.gpa, gpa) == 0 && name.equals(student.name);
-    }
-}
